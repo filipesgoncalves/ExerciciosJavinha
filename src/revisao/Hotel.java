6878d88a56b2170396420b21f5278b38c733ff6e
filipesgoncalves <filipesenragoncalves@gmail.com>
2025-09-15 01:01:06 -0300

    Revisao finalizada

--- src/revisao/Hotel.java
+++ src/revisao/Hotel.java
@@ -23,11 +23,11 @@ public class Hotel {
             if (quarto.getNumero() == numeroQuarto && !quarto.isOcupado()){
                 quarto.setOcupado(true);
                 quarto.setHospede(hospede);
-                System.out.println("Hospede: " + hospede.getNome() + " hospedado no quarto " + numeroQuarto);
+                System.out.println("Atualização: " + hospede.getNome() + " hospedado no quarto " + numeroQuarto);
                 return;
             }
         }
-        System.out.println("Não foi possivel achar um quarto no momento. Pedimos desculpas pelo inconveniente.");
+        System.out.println("O quarto " + numeroQuarto + " está ocupado ou não foi encontrado. Pedimos desculpas pelo inconveniente.");
     }
 
     public void checkout(int numeroQuarto) {
@@ -39,15 +39,24 @@ public class Hotel {
                 return;
             }
         }
-        System.out.println("O quarto requisitado se encontra vazio no momento ou não foi encontrado.");
+        System.out.println("O quarto " + numeroQuarto + " se encontra vazio no momento ou não foi encontrado. Pedimos desculpas pelo inconveniente.");
     }
 
     public void listarHospedes() {
         System.out.println("--- LISTA DE HOSPEDES ---");
+        int total = quartos.size();
+        int ocupado = 0;
+
         for (Quarto quarto : quartos) {
             if (quarto.isOcupado()) {
+                ocupado++;
                 System.out.println("Quarto " + quarto.getNumero() + " ocupado por " + quarto.getHospede().getNome() + ".");
             }
+        }
+        if (ocupado == 0){
+            System.out.println("Todos os quartos estão livres!");
+        } else if (ocupado == total) {
+            System.out.println("Todos os quartos estão ocupados!");
         }
     }
 
--- src/revisao/Main.java
+++ src/revisao/Main.java
@@ -9,13 +9,20 @@ public class Main {
         hotel.addQuarto(quarto);
         hotel.addQuarto(quarto2);
 
+        hotel.listarHospedes();
+
         System.out.println("=============================================");
 
         Hospede h1 = new Hospede("Filipe", "111.111.111-11");
         Hospede h2 = new Hospede("Filipe 2", "222.222.222-22");
 
         hotel.hospedar(h1, 123);
+        hotel.hospedar(h2, 123);
         hotel.hospedar(h2, 122);
+
+        System.out.println("=============================================");
+
+        hotel.listarHospedes();
 
         System.out.println("=============================================");
 
@@ -24,8 +31,6 @@ public class Main {
         System.out.println("=============================================");
 
         hotel.listarHospedes();
-
-        System.out.println("=============================================");
 
 
     }
